b6c2cc334a9cfc1d63233f3b15367698f78665ce
Shane Alcock <salcock@waikato.ac.nz>
2016-08-26 13:51:38 +1200

    Replace copyright blurb in all .h files to use LGPL
    Removed unused source files.

--- lib/tcp/tcp_protocols.h
+++ lib/tcp/tcp_protocols.h
@@ -1,3 +1,28 @@
+/*
+ *
+ * Copyright (c) 2011-2016 The University of Waikato, Hamilton, New Zealand.
+ * All rights reserved.
+ *
+ * This file is part of libprotoident.
+ *
+ * This code has been developed by the University of Waikato WAND
+ * research group. For further information please see http://www.wand.net.nz/
+ *
+ * libprotoident is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * libprotoident is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *
+ */
 #ifndef TCP_PROTOCOLS_H_
 #define TCP_PROTOCOLS_H_
 
