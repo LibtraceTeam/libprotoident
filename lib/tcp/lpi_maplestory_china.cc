e5b691db6dc6613e796d777b2c6246431c7b6600
Shane Alcock <salcock@waikato.ac.nz>
2017-07-03 17:09:20 +1200

    A few tweaks to existing rules
    Updated:
     * IPSharkk
     * Maplestory China
     * WeChat UDP

--- lib/tcp/lpi_maplestory_china.cc
+++ lib/tcp/lpi_maplestory_china.cc
@@ -46,6 +46,15 @@ static inline bool match_cms_hello(uint32_t payload, uint32_t len) {
 
 }
 
+static inline bool match_cms_alt(uint32_t payload, uint32_t len) {
+
+        if (len == 16) {
+                if (MATCH(payload, 0x0e, 0x00, 0xba, 0x00))
+                        return true;
+        }
+        return false;
+}
+
 static inline bool match_maplestory_china(lpi_data_t *data, lpi_module_t *mod UNUSED) {
 
         /* Can also restrict to ports 8585 and 8586 if required */
@@ -57,6 +66,16 @@ static inline bool match_maplestory_china(lpi_data_t *data, lpi_module_t *mod UN
 
         if (match_cms_hello(data->payload[1], data->payload_len[1])) {
                 if (data->payload_len[0] == 42)
+                        return true;
+        }
+
+        if (match_cms_alt(data->payload[0], data->payload_len[0])) {
+                if (data->payload_len[1] == 40)
+                        return true;
+        }
+
+        if (match_cms_alt(data->payload[1], data->payload_len[1])) {
+                if (data->payload_len[0] == 40)
                         return true;
         }
 
