32a3293279ac12679b016290023d4a73005d2b45
Shane Alcock <salcock@waikato.ac.nz>
2019-08-02 15:07:50 +1200

    Add support for buster to gitlab package builder
     * strip package version numbers from git tags before building
       packages
     * Fix missing body in 2.0.13 debian changelog entry

--- gitlab-build.sh
+++ gitlab-build.sh
@@ -6,6 +6,8 @@ export DEBEMAIL='packaging@wand.net.nz'
 export DEBFULLNAME='WAND Packaging'
 export DEBIAN_FRONTEND=noninteractive
 
+SOURCENAME=`echo ${CI_COMMIT_REF_NAME} | cut -d '-' -f 1`
+
 apt-get update
 apt-get install -y equivs devscripts dpkg-dev quilt curl apt-transport-https \
     apt-utils ssl-cert ca-certificates gnupg lsb-release debhelper git
@@ -18,6 +20,6 @@ curl --silent "https://bintray.com/user/downloadSubjectPublicKey?username=wand"\
 apt-get update
 apt-get upgrade -y
 
-dpkg-parsechangelog -S version | grep -q ${CI_COMMIT_REF_NAME} || debchange --newversion ${CI_COMMIT_REF_NAME} -b "New upstream release"
+dpkg-parsechangelog -S version | grep -q ${SOURCENAME} || debchange --newversion ${SOURCENAME} -b "New upstream release"
 mk-build-deps -i -r -t 'apt-get -f -y --force-yes'
 dpkg-buildpackage -b -us -uc -rfakeroot -j4
