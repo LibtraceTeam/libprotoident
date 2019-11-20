6186edbb6d9f9348ef101957fbe774b9ff5c659d
Shane Alcock <salcock@waikato.ac.nz>
2019-11-20 14:07:13 +1300

    Updated TCP Steam to include matches for VT01 traffic

--- lib/tcp/lpi_steam.cc
+++ lib/tcp/lpi_steam.cc
@@ -30,7 +30,7 @@
 #include "proto_manager.h"
 #include "proto_common.h"
 
-static inline bool match_steam(lpi_data_t *data, lpi_module_t *mod UNUSED) {
+static inline bool match_old_steam(lpi_data_t *data) {
 
 	/* Steam TCP Download */
 
@@ -57,11 +57,58 @@ static inline bool match_steam(lpi_data_t *data, lpi_module_t *mod UNUSED) {
 	return false;
 }
 
+static inline bool match_steam_len(uint32_t payload, uint32_t len) {
+
+        /* length is stored as a little-endian integer */
+#if BYTE_ORDER == BIG_ENDIAN
+        uint32_t plen = bswap32(payload);
+#else
+        uint32_t plen = payload;
+#endif
+
+        if (plen == len - 8) {
+                return true;
+        }
+        return false;
+}
+
+static inline bool match_steam_vt01(lpi_data_t *data) {
+
+        /* I imagine other ports are possible? */
+
+        if (data->server_port != 27050 && data->client_port != 27050) {
+                return false;
+        }
+
+        /* Observed length pairs so far:
+         *   150, 76
+         */
+
+        if (match_steam_len(data->payload[0], data->payload_len[0])) {
+                if (match_steam_len(data->payload[1], data->payload_len[1])) {
+                        return true;
+                }
+        }
+
+        return false;
+}
+
+static inline bool match_steam(lpi_data_t *data, lpi_module_t *mod UNUSED) {
+
+        if (match_old_steam(data))
+                return true;
+        if (match_steam_vt01(data)) {
+                return true;
+        }
+        return false;
+}
+
+
 static lpi_module_t lpi_steam = {
 	LPI_PROTO_STEAM,
 	LPI_CATEGORY_GAMING,
 	"Steam_TCP",
-	4, /* Might not be as reliable as some other rules (?) */
+	104, /* Might not be as reliable as some other rules (?) */
 	match_steam
 };
 
